e8664977b9d9f7a4dcc2e08201476aaa7559a58d
Liam Nichols <liam.nichols.ln@gmail.com>
2022-04-08 15:45:36 +0200

    Update Package.swift to conditionally include StaticInternalSwiftSyntaxParser as a dependency of SwiftSyntax

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.3
 
 import PackageDescription
 import Foundation
@@ -16,6 +16,21 @@ let package = Package(
   ]
 )
 
+// Include the parser library as a binary dependency if both the host and the target are macOS.
+//  - We need to require that the host is macOS (checked by `#if os(macOS)`) because package resolve will download and unzip the referenced framework, which requires `unzip` to be installed. Only macOS guarantees that `unzip` is installed, the Swift Docker images don’t have unzip installed, so package resolve would fail.
+//  - We need to require that the target is macOS (checked by `.when`) because binary dependencies are only supported by SwiftPM on macOS.
+#if os(macOS)
+let parserLibraryTargets: [Target] = [.binaryTarget(
+  name: "lib_InternalSwiftSyntaxParser",
+  url: "https://github.com/keith/StaticInternalSwiftSyntaxParser/releases/download/5.5.2/lib_InternalSwiftSyntaxParser.xcframework.zip",
+  checksum: "96bbc9ab4679953eac9ee46778b498cb559b8a7d9ecc658e54d6679acfbb34b8"
+)]
+let parserLibraryDependency: [Target.Dependency] = [.target(name: "lib_InternalSwiftSyntaxParser", condition: .when(platforms: [.macOS]))]
+#else
+let parserLibraryTargets: [Target] = []
+let parserLibraryDependency: [Target.Dependency] = []
+#endif
+
 let swiftSyntaxTarget: PackageDescription.Target
 
 /// If we are in a controlled CI environment, we can use internal compiler flags
@@ -32,13 +47,14 @@ if ProcessInfo.processInfo.environment["SWIFT_BUILD_SCRIPT_ENVIRONMENT"] != nil 
   // Disable it when we're in a controlled CI environment.
   swiftSyntaxUnsafeFlags += ["-enforce-exclusivity=unchecked"]
 
-  swiftSyntaxTarget = .target(name: "SwiftSyntax", dependencies: ["_CSwiftSyntax"],
+  swiftSyntaxTarget = .target(name: "SwiftSyntax", dependencies: ["_CSwiftSyntax"] + parserLibraryDependency,
                               swiftSettings: [.unsafeFlags(swiftSyntaxUnsafeFlags)]
   )
 } else {
-  swiftSyntaxTarget = .target(name: "SwiftSyntax", dependencies: ["_CSwiftSyntax"])
+  swiftSyntaxTarget = .target(name: "SwiftSyntax", dependencies: ["_CSwiftSyntax"] + parserLibraryDependency)
 }
 
+package.targets.append(contentsOf: parserLibraryTargets)
 package.targets.append(swiftSyntaxTarget)
 
 let libraryType: Product.Library.LibraryType
